b65cd49cedcfd242f30ae5211e049947154072a9
xiaolong Hao <2072451919@qq.com>
2020-02-24 14:05:43 +0800

    add some console function

--- src/utils/console.js
+++ src/utils/console.js
@@ -1,13 +1,153 @@
-/* eslint-disable */
+import consoleTool from './consoleTool'
+import * as judge from './judgeType'
 
-function exeCode(code) {
-  return `\ntry {\n` +
-    `console.log('${code}')\n` +
-    `var r = eval('${code}')\n` +
-    `console.log({data: r, type: 'return'})\n` +
-    `} catch(e) {\n` +
-    `console.log({data: e, type: 'error'})\n` +
-    `}\n`
+export default class Console {
+  constructor(iframe) {
+    if (!Console.instance) {
+      this.window = iframe.contentWindow
+      this.console = this.window.console
+      this.consoleInfo = []
+      this.ableMethods = ['log', 'info', 'warn', 'error']
+      this.init()
+      Console.instance = this
+    }
+    return Console.instance
+  }
+  /**
+   * 判断该方法是否可用
+   * @param String methodStr
+   * @return Boolean
+   */
+  judgeMethodsAllowed (methodStr) {
+    return this.ableMethods.indexOf(methodStr) > -1 ? true : false
+  }
+  /**
+   * 初始化控制台
+   */
+  init () {
+    const iframeConsole = this.console
+    // 重写window的onerror事件
+    this.window.onerror = (msg, _, row, col) => {
+      this.consoleInfo.push({
+        type: 'system-error',
+        content: msg,
+        row,
+        col
+      })
+      return true
+    }
+    // 重写console的一些方法
+    this.ableMethods.forEach(item => {
+      iframeConsole[item] = (...arg) => {
+        this.consoleInfo.push(
+          this.printLog({
+            type: item,
+            content: arg
+          })
+        )
+      }
+    })
+  }
+  /**
+   * 将日志打印到日志显示窗口
+   * @param Object item
+   * @param String type 日志类型  log|info|debug|error|warn 必要！
+   * @param Array content 日志内容
+   * @return finLog 最终显示在页面上的日志html
+   */
+  printLog (item) {
+    const type = item.type
+    const content = item.content
+    if (!this.judgeMethodsAllowed(type)) {
+      return {
+        type: 'error',
+        logs: `console.${type} is not a function`
+      }
+    }
+    let finLog = []
+    switch (type) {
+      case 'log':
+        finLog = {
+          type,
+          logs: this.log(content)
+        }
+        break
+      case 'error':
+
+    }
+    return finLog
+  }
+  /**
+   * 获取所有日志
+   * @return consoleInfo
+   */
+  getConsoleInfo () {
+    return this.consoleInfo
+  }
+  /**
+   * 生成带有log的html字符串
+   * @param Array content 输出内容
+   * @return finLog 最终显示在页面上的日志
+   */
+  log (content) {
+    const finLog = []
+    if (!content.length) return ''
+    content.forEach(item => {
+      const type = judge.judgeType(item)
+      let html = ''
+      switch (type) {
+        case 'string':
+          html = this.renderString(item)
+          break
+        case 'number':
+          html = this.renderNumber(item)
+          break
+        case 'boolean':
+          html = this.renderBoolean(item)
+          break
+        case 'symbol':
+          html = this.renderSymbol(item)
+          break
+        case 'null':
+          html = this.renderNull(item)
+          break
+        case 'undefined':
+          html = this.renderUndefined(item)
+          break
+        case 'Object':
+          html = this.renderObject(item)
+          break
+      }
+      finLog.push(html)
+    })
+    return finLog
+  }
+  /**
+   * 
+   * @param String str 
+   * @param Number num
+   * @return String html string
+   */
+  renderString (str) {
+    return `<span class="cm-string">"${str}"</span>`
+  }
+  renderNumber (num) {
+    return `<span class="cm-keyword">${num}</span>`
+  }
+  renderBoolean (bool) {
+    return `<span class="cm-keyword">${bool}</span>`
+  }
+  renderSymbol (sym) {
+    sym = String(sym)
+    return `<span style="color: #dd0a20">${sym}</span>`
+  }
+  renderNull (nul) {
+    return `<span class="js-encoder-console-null">${nul}</span>`
+  }
+  renderUndefined (und) {
+    return `<span class="js-encoder-console-undefined">${und}</span>`
+  }
+  renderObject (obj) {
+
+  }
 }
-
-export default exeCode
---
+++ src/utils/consoleTool.js
@@ -0,0 +1,117 @@
+import * as judge from './judgeType'
+
+/**
+ * 获取对象的所有键，包括不可枚举的键
+ * @param Object obj 
+ * @return Array
+ */
+function getObjAllKeys (obj) {
+  const type = judge.judgeType(obj)
+  if (type !== 'Object' && type !== 'Array') return []
+  if (type === 'Array') {
+    const arr = []
+    obj.forEach((_, index) => {
+      arr.push(index)
+    })
+    return arr
+  }
+  return Object.getOwnPropertyNames(obj).sort()
+}
+/**
+ * 将对象转化成字符串（最顶层的键值对）
+ * @param Object stringObject
+ * @return String
+ */
+function JSONStringify (stringObject) {
+  const type = judge.judgeType(stringObject)
+  if (type !== 'Object' && type !== 'Array') {
+    return JSON.stringify(stringObject)
+  }
+
+  let prefix = '{', suffix = '}'
+  if (type === 'Array') {
+    prefix = '['
+    suffix = ']'
+  }
+  let str = prefix
+
+  const keys = getObjAllKeys(stringObject)
+  for (let i = 0;i < keys.length;i++) {
+    const key = keys[i]
+    const value = stringObject[key]
+    try {
+      // key
+      if (type !== 'Array') {
+        const keyType = judge.judgeType(key)
+        switch (keyType) {
+          case 'Object':
+          case 'Array':
+          case 'Symbol':
+            str += Object.prototype.toString.call(key)
+            break
+          default:
+            str += key
+        }
+        str += ': '
+      }
+
+      // value
+      const valueType = judge.judgeType(value)
+      switch (valueType) {
+        case 'Array':
+          str += 'Array[' + value.length + ']'
+          break
+        case 'Object':
+        case 'Function':
+        case 'Symbol':
+          str += Object.prototype.toString.call(value)
+          break
+        default:
+          str += JSON.stringify(value)
+      }
+      if (i < keys.length - 1) str += ', '
+    } catch (e) {
+      continue
+    }
+  }
+  str += suffix
+  return str
+}
+/**
+ * 检测对象是否为普通对象（不包含window、结点、特殊对象：数组，函数等。。。）
+ * @param Object obj
+ * @return Boolean
+ */
+function judgePlainObj (obj) {
+  const hasOwnProp = Object.prototype.hasOwnProperty
+  if (!obj || typeof obj !== 'object' || obj.nodeType || judgeWindow(obj)) {
+    return false
+  }
+  try {
+    if (obj.constructor
+      && !hasOwnProp.call(obj, 'constructor')
+      && !hasOwnProp.call(obj.constructor.prototype, 'isPrototypeOf')) {
+      return false
+    }
+  } catch (e) {
+    return false
+  }
+  let key
+  for (key in obj) { }
+  return key === undefined || hasOwnProp.call(obj, key)
+}
+/**
+ * 判断对象是否为window
+ * @param Object obj
+ * @return Boolean
+ */
+function judgeWindow (obj) {
+  const type = judge.judgeType(obj)
+  return type === 'global' || type === 'Window' || type === 'DOMWindow'
+}
+
+export default {
+  getObjAllKeys,
+  JSONStringify,
+  judgePlainObj
+}
