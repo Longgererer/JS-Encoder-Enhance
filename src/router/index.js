2eb14a37edfee6781bf42029da7a7995d314329a
xiaolong Hao <2072451919@qq.com>
2020-03-06 21:01:27 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Editor from '@/components/editor.vue'
+import Profile from '@/components/profile.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -13,6 +14,11 @@ export default new Router({
       path: '/editor',
       name: 'editor',
       component: Editor
+    },
+    {
+      path: '/profile/:id',
+      name: 'profile',
+      component: Profile
     }
   ]
 })
