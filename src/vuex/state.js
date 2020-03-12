253cf1673572baf8d3e1c1563babad59ca8f15d4
xiaolong Hao <2072451919@qq.com>
2020-03-12 20:02:58 +0800

    Update state.js

--- src/vuex/state.js
+++ src/vuex/state.js
@@ -13,34 +13,44 @@ export default {
   codeAreaHeight: 0,
   // 代码窗口宽度
   codeAreaWidth: 0,
+  // 项目名称
+  projectName: '',
   // 预处理语言
   preprocess: [
     'HTML',
     'CSS',
     'JavaScript'
   ],
+  // 项目标签
+  tags:[],
   // 代码窗内容
   codeAreaContent: {
     HTML: '<div>\n\tHello World!\n</div>',
     CSS: '* {\n\tmargin: 0;\n\tpadding: 0;\n}',
     JavaScript: 'console.log(1)'
   },
+  // js外部链接
+  CDNList: [],
+  // css外部链接
+  linkList: [],
   // 是否显示遮罩层
   showBg: false,
   // 是否显示侧边用户信息栏
   showSlideUserMenu: false,
+  // 是否显示储存按钮
+  showSaveBtn: false,
+  // 是否显示储存提示
+  showSaveTip: true,
   // 用户登录状态
   loginStatus: false,
   // 设置项
   codeOptions: {
     replace: true,
     autoUp: true,
-    waitTime: 1000
+    waitTime: 1000,
+    showHistoryLog: false,
+    tabIndent: 2
   },
-  // js外部链接
-  CDNList: [],
-  // css外部链接
-  linkList: [],
   // iframe宽度
   iframeWidth: 0,
   // iframe遮罩层
